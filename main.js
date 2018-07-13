034304f3415d9d1d0a5a4bf975ec817a8e6791fa
TurquoiseTortoise <40045109+jianqi14@users.noreply.github.com>
2018-07-12 22:43:49 -0700

    addeded

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ $.getJSON( airtable_list_url, function(data) {
     $.each(data.records, function( key, val ){
         console.log(val.fields)
         items.push(`<h4>${val.fields['Name']} </h4>`)
+        items.push(`<h4>${val.fields['Photos']}</h4>`)
+        items.push(`<h4>${val.fields['Address']}</h4>`)
 
     });
     $(".list-view").append(items.join(''));
