30d1900a8a726eba2ef41ecac3bca8bf698aaa88
TurquoiseTortoise <40045109+jianqi14@users.noreply.github.com>
2018-07-12 18:42:58 -0700

    j

---
+++ main.js
@@ -0,0 +1,13 @@
+//tHIS url is fom airtable from the Authentication section
+var airtable_list_url = 'https://api.airtable.com/v0/appWNP5SS9OqdZq7D/Table%201?api_key=keyaK6MUiRbQVk9Di';
+
+//This is where we get the JSON data from Airtable!
+$.getJSON( airtable_list_url, function(data) {
+    var items = [];
+
+    $.each(data.records, function( key, val ){
+        console.log(val.fields)
+        items.push(`<h2>${val.fields['Name']} </h2>`)
+    });
+    $(".list-view").append(items.join(''));
+});
