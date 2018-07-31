9f1b93a38a6a60807ec47f3cf93d431619400235
Jian Qi <jianqi1997@gmail.com>
2018-07-31 02:45:00 -0700

    Huge Changes on HTML/CSS/Javascript

--- main.js
+++ main.js
@@ -2,41 +2,17 @@
 var airtable_list_url = 'https://api.airtable.com/v0/appWNP5SS9OqdZq7D/Table%201?api_key=keyaK6MUiRbQVk9Di';
 
 var cardTemplate = function(photos, name, description){
-    return `
-    <div class="card-deck">
-    <div class="card">
-      <img class="card-img-top" src="${photos}" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">${name}</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">Last updated 3 mins ago</small>
-      </div>
-    </div>
 
-    <div class="card">
-      <img class="card-img-top" src="${photos}" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">${name}</h5>
-        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">Last updated 3 mins ago</small>
-      </div>
-    </div>
-
-    <div class="card">
-      <img class="card-img-top" src="${photos}" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">${name}</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">Last updated 3 mins ago</small>
-      </div>
+  return `
+  <div class="card img-fluid" style="width:500px">
+    <img class="card-img-top" src="${photos}" alt="Card image" style="width:100%">
+    <div class="card-img-overlay">
+      <h4 class="card-title">${name}</h4>
+      <p class="card-text">${description}</p>
+      <a href="#" class="btn btn-primary">Click to see more!!</a>
     </div>
   </div>
+</div>
     `;
 }
 
@@ -48,7 +24,7 @@ $.getJSON( airtable_list_url, function(data) {
         var name = val.fields['Name'];
         var description = val.fields['Description'];
         var photos = val.fields['Photos'][0] ? val.fields['Photos'][0].url : null;
-        var html = cardTemplate(name, description, photos);
+        var html = cardTemplate(photos, name, description);
 
         items.push(html);
 
