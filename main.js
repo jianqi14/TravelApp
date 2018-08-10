4a972f52ab3b42b69230d36c2ba207dadd3f6233
Jian Qi <jianqi1997@gmail.com>
2018-08-10 02:03:43 -0700

    upload to Git

--- main.js
+++ main.js
@@ -1,29 +1,40 @@
-var listView = function(photos, name, description, id){
+// Helper for getting the `index.html?ID=` part form the URL
+var getParameterByName = function(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"), results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
+// Airtable API Key, unique per user
+var api_key = 'keyaK6MUiRbQVk9Di';
+
+var listView = function(id, photos, name){
   return `
   <div class="card img-fluid" style="width:500px">
     <img class="card-img-top" src="${photos}" alt="Card image" style="width:100%">
     <div class="card-img-overlay">
       <h4 class="card-title">${name}</h4>
-      <p class="card-text">${description}</p>
-      <a href="index.html?id=${id}" class="btn btn-primary">${name} Bio</a>
+      <p class="card-text"><a href="index.html?id=${id}" class="btn btn-primary">${name} Bio</a></p>
+      
     </div>
   </div>
   <br>
-</div>
-    `;
+</div>`;
 }
 
 var getDataForList = function(){
   //This is where we get the JSON data from Airtable!
-  $.getJSON( `https://api.airtable.com/v0/appWNP5SS9OqdZq7D/Table%201?api_key=keyaK6MUiRbQVk9Di`, function(data) {
+  $.getJSON( `https://api.airtable.com/v0/appWNP5SS9OqdZq7D/Table%201?api_key=keyaK6MUiRbQVk9Di&view=Alpha`, function(data) {
     var items = [];
-    $.each(data.records, function( index, val ){
+    $.each(data.records, function(index, val ){
         //console.log(val.fields)
-        var name = val.fields['Name'];
-        var description = val.fields['Description'];
+        var id = val.id;
+        var name = val.fields['Name']; 
         var photos = val.fields['Photos'][0] ? val.fields['Photos'][0].url : null;
-        var html = listView(photos, name, description);
+        var html = listView(id, photos, name);
 
         items.push(html);
 
@@ -33,41 +44,38 @@ var getDataForList = function(){
 }
 
 // Template that generates HTML for one item in our detail view, given the parameters passed in
-var detailView = function(id, Photos, name, Description, address, YelpReview) {
-  return `<div class="col-sm-12">
-  <div class="card mb-4 box-shadow">
-    <img class="card-img-top" src="${Photos}">
-    <div class="card-body">
-      <h2>${name}</h2>
-      <p class="card-text">${Description}</p>
-      <p class="card-text">${address}</p>
-      <div class="d-flex justify-content-between align-items-center">
-        <small class="text-muted">${YelpReview}</small>
-        
-      </div>
-      
-    </div>
-  </div>
-</div>`;
+var detailView = function(id, Photos, name, Description, address, YelpReview, Price) {
+  return `
+  <div class="card mb-3">
+  <img class="card-img-top" src="${Photos}" alt="Card image cap">
+  <div class="card-body">
+    <h3 class="card-title"><strong>${name}</strong></h3>
+    <p class="card-text">${Description}</p>
+    <p class="card-text"><strong>${name}'s Address:</strong> ${address}</p>
+    <p class="card-text"><strong>Price:</strong> ${Price}</p>
+
+    <a class="btn btn-primary btn-lg" href="${YelpReview}" role="button">Yelp Review</a>
+    <a class="btn btn-primary btn-lg" href="https://www.google.com/maps/search/${name} ${address}" role="button"> Google Maps </a>
+  </div>`;
 }
 
 // Get and display the data for one item based on on the ID
 var getDataForId = function(id) {
-  $.getJSON(`https://api.airtable.com/v0/appWNP5SS9OqdZq7D/Table%201/${id}?api_key=${api_key}`, function( record ) {
+  $.getJSON(`https://api.airtable.com/v0/appWNP5SS9OqdZq7D/Table%201/${id}?api_key=keyaK6MUiRbQVk9Di`, function( record ) {
     //console.log(data);
     var html = [];
     html.push(`<div class="row">`);
-      console.log(val)
       var id = record.id;
       var fields = record.fields;
       var name = fields["Name"];
 
       var Photos = fields["Photos"] ? fields["Photos"][0].url : '';
-      var Description = fields["description"];
+      var Description = fields["Description"];
       var address = fields["Address"];
       var YelpReview = fields["yelpReviews"];
+      var Price = fields["Prices"];
 
-      var itemHTML = detailView(id, Photos, name, Description, address, YelpReview);
+      var itemHTML = detailView(id, Photos, name, Description, address, YelpReview, Price);
       html.push(itemHTML);
     html.push(`</div>`);
     $(".detail-view").append(html.join(""));
@@ -85,3 +93,4 @@ if (id) {
 } else {
   getDataForList();
 }
+
